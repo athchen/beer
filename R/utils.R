b283827adc190b8168f6e3e89e16b656a1d7f07e
Athena Chen <achen70@jhu.edu>
2021-08-05 20:16:35 -0500

    Updated workflows

--- R/utils.R
+++ R/utils.R
@@ -1,18 +1,6 @@
-#' @export
 .checkCounts <- function(object){
     is_int <- vapply(PhIPData::counts(object), is.integer, logical(1))
     if(!all(is_int)){
         stop("`counts` entries must be integers.")
     }
 }
-
-#' @export
-getPropReads <- function(object){
-
-    n <- PhIPData::librarySize(object, withDimnames = FALSE)
-    n_matrix <- matrix(rep(n, nrow(object)), nrow = nrow(object), byrow = TRUE)
-    counts_matrix <- as.matrix(PhIPData::counts(object))
-
-    counts_matrix/n_matrix
-}
-
